40a7e90847f99ecaf7ede19be33fcd22349d2544
Pranav Korhale <korhalepranav@gmail.com>
2025-02-27 18:47:03 +0530

    change in code=> commit for PRONOB static site

--- src/server.py
+++ src/server.py
@@ -7,4 +7,8 @@ def serve_index():
     return send_from_directory("static", "index.html")
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=8080, debug=True)  # No SSL
+=======
+    app.run(host="0.0.0.0", port=8080, debug=True)  # No SSL
+>>>>>>> 66de6c4 (change in code=> commit for PRONOB static site)
