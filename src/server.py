7a47f31928bb24984862c501336f5b16bb4f16ad
Pranav Korhale <45190350+pranavkorhale@users.noreply.github.com>
2025-02-27 18:33:00 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route("/")
-def home():
-    return "<h1>PRONOB Secure Server is Running!</h1>"
+def serve_index():
+    return send_from_directory("static", "index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)  # Removed SSL context
+    app.run(host="0.0.0.0", port=8080, debug=True)  # No SSL
