3faf7b7a34ad1af01abe44bf99b24faa910ce483
Pranav Korhale <45190350+pranavkorhale@users.noreply.github.com>
2025-02-27 18:29:50 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,13 +1,10 @@
 from flask import Flask
-import ssl
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>PRONOB Secure Server is Running with SSL!</h1>"
+    return "<h1>PRONOB Secure Server is Running!</h1>"
 
 if __name__ == "__main__":
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    
-    app.run(host="0.0.0.0", port=8080, debug=True, ssl_context=context)
+    app.run(host="0.0.0.0", port=8080, debug=True)  # Removed SSL context
