8c271b7b0759c42e1a8a98c260e5222a8440e037
Pranav Korhale <korhalepranav@gmail.com>
2025-02-27 18:06:51 +0530

    change in code=> commit for PRONOB static site

--- src/server.py
+++ src/server.py
@@ -1,11 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask
+import ssl
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
-def serve_index():
-    return send_from_directory("static", "index.html")
+def home():
+    return "<h1>PRONOB Secure Server is Running with SSL!</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
-
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile="certificate.pem", keyfile="privateKey.pem")
+    
+    app.run(host="0.0.0.0", port=8080, debug=True, ssl_context=context)
