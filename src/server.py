9115341af3e7191d285dc690be3a8eda62ec8262
Pranav Korhale <45190350+pranavkorhale@users.noreply.github.com>
2025-02-27 18:56:50 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -4,11 +4,7 @@ app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def serve_index():
-    return send_from_directory("static", "index.html")
+    return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(host="0.0.0.0", port=8080, debug=True)  # No SSL
-=======
-    app.run(host="0.0.0.0", port=8080, debug=True)  # No SSL
->>>>>>> 66de6c4 (change in code=> commit for PRONOB static site)
+    app.run(host="0.0.0.0", port=8080, debug=True)
